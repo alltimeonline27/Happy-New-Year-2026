a1a23f44a007ed5b6c29ef9cc825f936186e2164
BHASKAR CHARAN <alltimeonline.official@gmail.com>
2025-11-27 11:28:38 +0530

    v21.1

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // NY 2026 — OPTIMIZED SERVICE WORKER
 // ===============================
 
-const CACHE_NAME = "ny2026-v21";
+const CACHE_NAME = "ny2026-v21.1";
 
 // Only cache GET static files
 const ASSETS = [
